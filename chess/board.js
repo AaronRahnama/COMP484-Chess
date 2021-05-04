d035d6ccd07b8e3ad12e9dadfd06ce8339ab84d7
RidgeGT <Ridge.Tejuco.881@my.csun.edu>
2021-05-03 19:56:02 -0700

    - fixed move validation - added socket move handling from p2p - added en passant and pawn promotion GUI - added join T

--- chess/board.js
+++ chess/board.js
@@ -1,62 +1,227 @@
+
+// Ridge Tejuco
+// Chess.js documentation: https://github.com/jhlywa/chess.js/blob/master/README.md
+// Chessboard.js Documentation: https://chessboardjs.com/docs#methods
+
 function run_display() {
+    const socket = io();
     const game = new Chess();
-    function onDragStart(source, piece, position, orientation) {
-        console.log('Drag started:');
-        console.log('Source: ' + source);
-        console.log('Piece: ' + piece);
-        console.log('Position: ' + Chessboard.objToFen(position));
-        console.log('Orientation: ' + orientation);
-        console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-        toggleValidMoves(source);
-    }
-
-    function toggleValidMoves(source) {
-        let moves = game.moves({square:source,verbose:true});
-        for(let i = 0;i < moves.length;i++)
-        {
-            console.log(moves[i]);
-            let $square = $('.square-' + moves[i].to);
-            $square.toggleClass('dot');
-        }
-    }
-
-    function onDrop(source, target, piece, newPos, oldPos, orientation) {
-        console.log('Source: ' + source);
-        console.log('Target: ' + target);
-        console.log('Piece: ' + piece);
-        console.log('New position: ' + Chessboard.objToFen(newPos));
-        console.log('Old position: ' + Chessboard.objToFen(oldPos));
-        console.log('Orientation: ' + orientation);
-        console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-        toggleValidMoves(source);
-        // see if the move is legal
-        let move = game.move({
-            from: source,
-            to: target,
-            promotion: 'q' // NOTE: always promote to a queen for example simplicity
-        })
-        // illegal move
-        if (move === null) return 'snapback'
-    }
-
-    function onDragMove(newLocation, oldLocation, source,
-        piece, position, orientation) {
-        console.log('New location: ' + newLocation)
-        console.log('Old location: ' + oldLocation)
-        console.log('Source: ' + source)
-        console.log('Piece: ' + piece)
-        console.log('Position: ' + Chessboard.objToFen(position))
-        console.log('Orientation: ' + orientation)
-        console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-    }
-
-    const config = {
+    let config = {
         draggable: true,
         dropOffBoard: 'snapback',
         onDragStart: onDragStart,
-        onDragMove: onDragMove,
+        onSnapEnd: onSnapEnd,
         onDrop: onDrop,
+        orientation: 'white',
+        //sparePieces: true,
         position: 'start'
     };
+
+    const urlQuery = new URLSearchParams(window.location.search);
+    //https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams
+    let game_id = "";
+    if(urlQuery.has("game_id"))
+    {
+        config.orientation = 'black';
+        game_id = urlQuery.get("game_id");
+    }
+    else
+    {
+        game_id = getCookie('game_id');
+    }
+
+    
+
     const board = Chessboard('board1', config);
+    $(window).resize(board.resize);
+
+    let promoting = false;
+    let promotion_src = '';
+    let promotion_tgt = '';
+
+    // ========================= Send Join message for Game room ==========================
+    socket.on("connect", () => {
+        //console.log("Checking Connection...");
+        //console.log(socket.connected);
+        socket.emit("join",game_id);
+        // show connection
+    });
+
+
+    // ========================= Handle incoming game moves ==========================
+    socket.on("move", (data) => {
+        console.log(data)
+        let move = game.move({
+            from: data.SRC,
+            to: data.TGT,
+            promotion: data.PRM // NOTE: always promote to a queen for example simplicity
+        });
+
+        // check if illegal move
+        if (move === null) return 'snapback'
+        else board.position(game.fen()); //update board
+    });
+
+
+
+
+    function getCookie(name) {
+        //https://stackoverflow.com/questions/10730362/get-cookie-by-name
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) return parts.pop().split(';').shift();
+    }
+
+    //console.log("game_id:" + game_id);
+
+    // ==================== Start of chess UI code ========================
+
+
+    function onDragStart(source, piece, position, orientation) {
+        // console.log('Drag started:');
+        // console.log('Source: ' + source);
+        // console.log('Piece: ' + piece);
+        // console.log('Position: ' + Chessboard.objToFen(position));
+        // console.log('Orientation: ' + orientation);
+        // console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+        if (!promoting) {
+            toggleValidMoves(source);
+        }
+    }
+
+    function toggleValidMoves(source) {
+        let moves = game.moves({ square: source, verbose: true });
+        //console.log(moves);
+        let last_move = '';
+        for (let i = 0; i < moves.length; i++) {
+            //console.log(moves[i]);
+            let $square = $('.square-' + moves[i].to);
+            if (last_move != moves[i].to) {
+                $square.toggleClass('dot');
+            }
+            last_move = moves[i].to;
+        }
+        return moves;
+    }
+
+    function onDrop(source, target, piece, newPos, oldPos, orientation) {
+        if (!promoting) {
+            let moves = toggleValidMoves(source);
+            // see if the move is legal
+            if (is_promotion(piece, target) && containsSquare(moves, target)) {
+                //check if promotion is valid
+                promotion_tgt = target;
+                promotion_src = source;
+                console.log(promotion_tgt + promotion_src);
+                togglePromotionBox();
+            }
+            else {
+                makeMove(source, target, 'q');
+            }
+        }
+    }
+
+    function containsSquare(moveList, this_square) {
+        let ans = false;
+        for (let i = 0; i < moveList.length; i++) {
+            if (moveList[i].to == this_square) {
+                ans = true;
+                break;
+            }
+        }
+        return ans;
+    }
+
+    function onSnapEnd() {
+        board.position(game.fen());
+    }
+
+    function makeMove(src, tgt, prm) {
+        let move = game.move({
+            from: src,
+            to: tgt,
+            promotion: prm
+        });
+        // illegal move
+        if (move === null) {
+            return 'snapback'
+        }
+        else {
+            //console.log("EMIT");
+            socket.emit("move", { SRC: src, TGT: tgt, PRM: prm, RM: game_id }, (response) => {
+                console.log(response.status);
+            });
+        }
+    }
+
+    // ====================== Promotion box ====================
+    function togglePromotionBox() {
+
+        if (!promoting)
+        {
+            promoting = true;
+            $("#promotionBox").css("visibility", "visible");
+        }
+        else
+        {
+            promoting = false;
+            $("#promotionBox").css("visibility", "hidden");
+        }
+    }
+
+    function is_promotion(piece, target) {
+        //console.log("Checking promotion");
+        let rank = target.slice(1, 2);
+        let type = piece.slice(1, 2);
+        //console.log(rank + piece);
+        let ans = false;
+        if (type == 'P') {
+            if (rank == '8' || rank == '1') {
+                ans = true;
+            }
+        }
+        return ans;
+    }
+
+    const promotion_text = document.getElementsByClassName("promotionText")[0];
+
+    // ========================= Change promotion text ======================
+    $('#knightButton').hover(function () {
+        promotion_text.innerHTML = "Promotion: Knight";
+    });
+    $('#queenButton').hover(function () {
+        promotion_text.innerHTML = "Promotion: Queen";
+    });
+    $('#rookButton').hover(function () {
+        promotion_text.innerHTML = "Promotion: Rook";
+    });
+    $('#bishopButton').hover(function () {
+        promotion_text.innerHTML = "Promotion: Bishop";
+    });
+    $('#cancelButton').hover(function () {
+        promotion_text.innerHTML = "Promotion: Cancel";
+    });
+
+    // ========================== Handle Promotion Choice ==================
+    $('#knightButton').click(function () {
+        updatePromotion('n');
+    });
+    $('#queenButton').click(function () {
+        updatePromotion('q');
+    });
+    $('#rookButton').click(function () {
+        updatePromotion('r');
+    });
+    $('#bishopButton').click(function () {
+        updatePromotion('b');
+    });
+    $('#cancelButton').click(function () {
+        togglePromotionBox();
+    });
+
+    function updatePromotion(choice) {
+        makeMove(promotion_src, promotion_tgt, choice)
+        board.position(game.fen());
+        togglePromotionBox();
+    }
 }
--- chess_server.js
+++ chess_server.js
@@ -1,13 +1,20 @@
-//npm install chess
+//npm install chess.js
 //npm install express
 //npm install nodemon
 //npm install socket.io
+//npm install uuid
 
+const portNUM = 3000;
 const express = require('express');
 const app = express();
+const { v4: uuidv4 } = require('uuid');
+const httpServer = require("http").createServer(app);
 const { Chess } = require('./chess.js')
-const io = require('socket.io')(80);
+const io = require('socket.io')(httpServer);
+
 //const socket = io("http://localhost");
+
+// read chess.html file into memory
 
 
 let GAME_COUNT = 0;
@@ -25,35 +32,103 @@ app.get('/create',(req,res)=>{
 });
 
 app.get('/testgame',(req,res)=>{ 
+    res.cookie('game_id',"7da40023-f8dc-4f98-9522-73ec87f19712",{sameSite:'strict'});
+    res.sendFile(__dirname + "/chess/chess.html");
+});
+
+app.get('/join',(req,res)=>{ 
+    let game_id = req.query.game_id;
+    //req.cookie('game_id',game_id)
+    console.log(game_id);
     res.sendFile(__dirname + "/chess/chess.html");
 });
 
 app.post('/create',(req,res) => {
-    //CreateGame(req.body.user);
+    let gid = CreateGame(req.body.user);
+    res.cookie('game_id',gid,{sameSite:'strict'});
+    res.sendFile(__dirname + "/chess/chess.html");
     console.log(req.body.user);
 });
 
 //=================================================== New Game ===================================================
-function CreateGame()
+function CreateGame(name)
 {
-    //TODO: CREATE socket connections and associate socket connection to game number
-    GAME_ID[GAME_COUNT] = uuidv4();
-    GAME[GAME_COUNT] = new Chess();
+    //TODO: SEND GAME FILES
+    let this_id = uuidv4();
+    //GAME_ID[GAME_COUNT] = this_id;
+    //GAME[GAME_COUNT] = new Chess();
     GAME_COUNT++;
+    return this_id;
 }
+
+function findIndexByGameID (game_id)
+{
+    for(let ii = 0; ii < GAME_ID.length; ii++)
+    {
+        if(GAME_ID[ii] == game_id)
+        {
+            break;
+        }
+    }
+    return ii;
+}
+
+function removeGame(index)
+{
+    //GAME_ID.splice(index,1);
+    //GAME.splice(index,1);
+    GAME_COUNT--;
+}
+
+
+// =================================================== Log Room messages ===================================================
+io.of("/").adapter.on("create-room", (room) => {
+    console.log(`room ${room} was created`);
+});
+  
+io.of("/").adapter.on("join-room", (room, id) => {
+    console.log(`socket ${id} has joined room ${room}`);
+});
+
+io.of("/").adapter.on("leave-room", (room, id) => {
+    console.log(`socket ${id} has left room ${room}`);
+});
+
+io.of("/").adapter.on("delete-room", (room, id) => {
+    removeGame();
+    console.log(`room ${room} was deleted`);
+});
 
 
 // TODO:
 // =================================================== Handle socket messages ===================================================
 
-// handle move from client (check if valid)
+io.on("connection", socket =>
+{
 
-// send move to client
+    socket.on("join",(id) =>
+    {
+        socket.join(id);
+    });
 
-//=================================================== LISTENER ===================================================
-const portNUM = 3000;
-app.listen(portNUM,()=>{
-    console.log('Listening on ' + portNUM);
+    socket.on("move", (data) =>
+    {
+        console.log("Move Received.");
+        console.log(data);
+        // handle move from client (check if valid)
+            //if move is valid send move
+        socket.to(data.RM).emit("move",{SRC: data.SRC, TGT: data.TGT, PRM: data.PRM});
+    });
+});
+
+//=================================================== LISTENERS ===================================================
+
+
+// app.listen(portNUM,()=>{
+//     console.log('Listening on ' + portNUM);
+// });
+httpServer.listen(portNUM, () => {
+    console.log("Listening on Port " + portNUM);
 });
 
 //=================================================== Restart timestamp ===================================================
